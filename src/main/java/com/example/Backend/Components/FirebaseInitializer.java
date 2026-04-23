765c2b8bc6f397db8042f0e569db50c6df02e527
Heng <Heng@DESKTOP-9E6M5NJ>
2026-04-23 12:09:30 +0700

    clean project

--- src/main/java/com/example/Backend/Components/FirebaseInitializer.java
+++ src/main/java/com/example/Backend/Components/FirebaseInitializer.java
@@ -6,25 +6,30 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FirebaseInitializer {
 
-//    @PostConstruct
-//    public void initialize() throws IOException {
-//        InputStream serviceAccount = getClass().getClassLoader()
-//                .getResourceAsStream("");
-//
-//        if (FirebaseApp.getApps().isEmpty()) {
-//            FirebaseOptions options = FirebaseOptions.builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .build();
-//            FirebaseApp.initializeApp(options);
-//        }
-//
-//        System.out.println("Firebase Initialized Successfully!");
-//    }
+    @PostConstruct
+    public void initialize() throws IOException {
+
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+
+        InputStream serviceAccount = new ByteArrayInputStream(
+                firebaseConfig.getBytes(StandardCharsets.UTF_8)
+        );
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+    }
 }
 
